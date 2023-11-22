c9c7fb7a2f72113c2f2d6c4ad0b71b7c2bb083ae
KelvinMomanyi <kelvinmata42@gmail.com>
2023-11-22 10:31:25 +0300

    new

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//import images as relative image path won't work with vite/vercel.
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+const trendingSushis = [
+    'Make Sushi',
+    'Nigiri Sushi',
+    'Oshizushi',
+    'Temaki Sushi',
+    'Uramaki Sushi',
+    'Inari Sushi'
+];
